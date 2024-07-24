170c544ec8fb1b81ce769629d500a6649acc212a
sdk250 <520sdk250@gmail.com>
2024-07-24 18:48:11 +0800

    优化代码整体框架，精简代码量

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # 介绍
 这是一个整合了 [`thread_socket`](https://github.com/sdk250/socket) 和 [`v2ray`](https://github.com/v2fly/v2ray-core) 的全局代理脚本。<br>
 为了仓库的精简，我省略了二进制程序，你也可以通过上面给出的仓库自行编译后放在 `Tools/` 目录下。<br>
-当然你也依然可以从 `Releases` 下载编译好的版本 <br>
+当然你也依然可以从 [`Releases`](https://github.com/sdk250/MLKit/releases) 下载编译好的版本 <br>
 从全局代理的角度出发，它很轻量，对于 `t模式` 仅使用了一个 `thread_socket` 来实现效果; 即使是具有分流功能的 `v模式` 也仅使用了 `v2ray` 来实现tun网卡转发流量。
 # 功能
 目前拥有两种模式
@@ -9,21 +9,24 @@
 出口为 `thread_socket` ，仅有中国的IP
 ## `v` 模式
 出口为 `v2ray` 与 `thread_socket` ，即拥有中国的IP地址与v2ray(自配置)的IP。
+## `s` 模式
+查看 `MLKit` 的运行状态
 # 使用方法 & 配置修改
 ```shell
 mlk.sh <mode>
 ```
-`mode` 为 `t` 或 `v`。
+`mode` 为 `t` 、`s` 或 `v`。
 ## 配置修改
 - 对于 `_v2.json` ，有<br>
+修改或增加服务器在 `outbounds` 这个键中,第30行开始<br>
 ```json
 {
     "tag": "global-out0",
     "protocol": "vmess",
     "settings": {
         "address": "0.0.0.0", // 更改为你的服务器
-        "port": 80, //
-        "uuid": "f0b4fdce-b506-11ee-8f87-2f0cc72f6658" //
+        "port": 80, // 对应端口
+        "uuid": "f0b4fdce-b506-11ee-8f87-2f0cc72f6658" // 对应UUID
     },
     "streamSettings": {
         "transport": "ws",
@@ -40,7 +43,8 @@ mlk.sh <mode>
 }
 ```
 仅需修改 `address` , `port` , `uuid` 为自己的特定配置项。<br>
-剩余的 `cn-out*` 以此类推。<br>
+剩余的 `cn-out*` 以此类推，只需要取消注释依照上面修改即可。<br>
 路由配置项参考[官方文档](https://www.v2fly.org/v5/config/router.html)。<br>
+默认为国内外分流出口<br>
 - 对于 `mlk.sh` ，有<br>
-我大部分写有注释。
+我大部分写有注释，请到文件内查看.
--- mlk.sh
+++ mlk.sh
@@ -3,7 +3,7 @@
 home_path="${0%/*}/Tools"
 
 # thread_socket 连接的IP(百度系)
-SERVER_ADDR="110.242.70.69"
+SERVER_ADDR="157.0.148.53"
 
 # Allow IP
 ALLOW_IP="127.0.0.0/8 \
@@ -14,10 +14,10 @@ ALLOW_IP="127.0.0.0/8 \
 	192.168/16 \
 	${SERVER_ADDR}/32"
 
-# Only for Android operation system
+# 仅适用于Android系统
 PACKAGES="/data/system/packages.list"
 
-# The package name of application you need allow
+# 在 Android 系统中的需要放行的应用的包名
 ALLOW_PACKAGES="com.android.bankabc \
 	com.nasoft.socmark \
 	com.v2ray.ang \
@@ -28,8 +28,32 @@ ALLOW_UDP_PACKAGES="com.tencent.tmgp.pubgmhd \
 	com.tencent.tmgp.sgame \
 	com.miHoYo.Yuanshen"
 
+# 适用于 Linux，需要放行的UID
+ALLOW_ALL_UID=''
+
+# UDP 放行
+ALLOW_UDP_UID=''
+
+# 放行本机DNS
+ALLOW_LOCAL_DNS=1
+
+# 放行热点DNS
+ALLOW_REMOTE_DNS=1
+
+# 放行本机UDP
+ALLOW_LOCAL_UDP=0
+
+# 放行本机TCP
+ALLOW_LOCAL_TCP=0
+
+# 放行热点UDP
+ALLOW_REMOTE_UDP=0
+
+# 放行热点TCP
+ALLOW_REMOTE_TCP=0
+
 # 需要放行的网卡，添加 wlan+ 进入可以放行Wifi
-ALLOW_LOOKUP="tun+ lo"
+ALLOW_LOOKUP='tun+ lo'
 
 # Be care for using
 ALLOW_UID=0
@@ -37,15 +61,14 @@ ALLOW_UID=0
 ALLOW_PORT=20822
 TCP_PORT=20802
 MARK=10086
-TUNDEV="tunDev"
+TUNDEV='tunDev'
 TABLE=101
 PREF=100
-TUN_ADDR="172.24.0.1/30"
+TUN_ADDR='172.24.0.1/30'
+WAIT_TIME=3
 
-v2ray_open() {
-	echo 1 > /proc/sys/net/ipv4/ip_forward
-	echo 1 > /proc/sys/net/ipv4/ip_dynaddr
-
+generate_uid()
+{
 	echo -e "# This file is automatical" \
 	"genrated by \`mlk\`\n# DO NOT edit it\n" > ${home_path}/.uid
 
@@ -58,6 +81,7 @@ v2ray_open() {
 			echo -n "${uid} " >> ${home_path}/.uid
 		fi
 	done
+	echo -n "${ALLOW_ALL_UID} " >> ${home_path}/.uid
 	echo -e -n "\nudp_uid=" >> ${home_path}/.uid
 	for PACKAGE in ${ALLOW_UDP_PACKAGES}
 	do
@@ -67,7 +91,37 @@ v2ray_open() {
 			echo -n "${uid} " >> ${home_path}/.uid
 		fi
 	done
+	echo -n "${ALLOW_UDP_UID} " >> ${home_path}/.uid
 
+	# Saving configuration
+	echo -e -n '\n' >> ${home_path}/.uid
+	echo "SERVER_ADDR=${SERVER_ADDR}" >> ${home_path}/.uid
+	echo "ALLOW_IP=${ALLOW_IP}" >> ${home_path}/.uid
+	echo "PACKAGES=${PACKAGES}" >> ${home_path}/.uid
+	echo "ALLOW_PACKAGES=${ALLOW_PACKAGES}" >> ${home_path}/.uid
+	echo "ALLOW_UDP_PACKAGES=${ALLOW_UDP_PACKAGES}" >> ${home_path}/.uid
+	echo "ALLOW_ALL_UID=${ALLOW_ALL_UID}" >> ${home_path}/.uid
+	echo "ALLOW_UDP_UID=${ALLOW_UDP_UID}" >> ${home_path}/.uid
+	echo "ALLOW_LOCAL_DNS=${ALLOW_LOCAL_DNS}" >> ${home_path}/.uid
+	echo "ALLOW_REMOTE_DNS=${ALLOW_REMOTE_DNS}" >> ${home_path}/.uid
+	echo "ALLOW_LOCAL_UDP=${ALLOW_LOCAL_UDP}" >> ${home_path}/.uid
+	echo "ALLOW_LOCAL_TCP=${ALLOW_LOCAL_TCP}" >> ${home_path}/.uid
+	echo "ALLOW_REMOTE_UDP=${ALLOW_REMOTE_UDP}" >> ${home_path}/.uid
+	echo "ALLOW_REMOTE_TCP=${ALLOW_REMOTE_TCP}" >> ${home_path}/.uid
+	echo "ALLOW_LOOKUP=${ALLOW_LOOKUP}" >> ${home_path}/.uid
+	echo "ALLOW_UID=${ALLOW_UID}" >> ${home_path}/.uid
+	echo "ALLOW_PORT=${ALLOW_PORT}" >> ${home_path}/.uid
+	echo "TCP_PORT=${TCP_PORT}" >> ${home_path}/.uid
+	echo "MARK=${MARK}" >> ${home_path}/.uid
+	echo "TUNDEV=${TUNDEV}" >> ${home_path}/.uid
+	echo "TABLE=${TABLE}" >> ${home_path}/.uid
+	echo "PREF=${PREF}" >> ${home_path}/.uid
+	echo "TUN_ADDR=${TUN_ADDR}" >> ${home_path}/.uid
+	echo "WAIT_TIME=${WAIT_TIME}" >> ${home_path}/.uid
+}
+
+create_tun()
+{
 	if [ -c /dev/tun ]
 	then
 		[ -f /dev/net/tun ] || ( mkdir -p /dev/net \
@@ -77,90 +131,247 @@ v2ray_open() {
 			&& mknod /dev/net/tun c 10 200 \
 			&& chmod 600 /dev/net/tun )
 	fi
+}
 
-	iptables -t filter -I FORWARD 1 \
-		-w 5 \
-		-i tun+ \
-		-j ACCEPT
-	iptables -t filter -I FORWARD 1 \
-		-w 5 \
-		-o tun+ \
-		-j ACCEPT
+find_configuration()
+{
+	echo $(grep -E '^[^#]' ${home_path}/.uid | \
+		grep -E "^${1}=" | awk -F= '{print $2}' \
+	)
+}
+
+load_configuration()
+{
+	SERVER_ADDR=$(find_configuration SERVER_ADDR)
+	ALLOW_IP=$(find_configuration ALLOW_IP)
+	PACKAGES=$(find_configuration PACKAGES)
+	ALLOW_PACKAGES=$(find_configuration ALLOW_PACKAGES)
+	ALLOW_UDP_PACKAGES=$(find_configuration ALLOW_UDP_PACKAGES)
+	ALLOW_ALL_UID=$(find_configuration ALLOW_ALL_UID)
+	ALLOW_UDP_UID=$(find_configuration ALLOW_UDP_UID)
+	ALLOW_LOCAL_DNS=$(find_configuration ALLOW_LOCAL_DNS)
+	ALLOW_REMOTE_DNS=$(find_configuration ALLOW_REMOTE_DNS)
+	ALLOW_LOCAL_UDP=$(find_configuration ALLOW_LOCAL_UDP)
+	ALLOW_LOCAL_TCP=$(find_configuration ALLOW_LOCAL_TCP)
+	ALLOW_REMOTE_UDP=$(find_configuration ALLOW_REMOTE_UDP)
+	ALLOW_REMOTE_TCP=$(find_configuration ALLOW_REMOTE_TCP)
+	ALLOW_LOOKUP=$(find_configuration ALLOW_LOOKUP)
+	ALLOW_UID=$(find_configuration ALLOW_UID)
+	ALLOW_PORT=$(find_configuration ALLOW_PORT)
+	TCP_PORT=$(find_configuration TCP_PORT)
+	MARK=$(find_configuration MARK)
+	TUNDEV=$(find_configuration TUNDEV)
+	TABLE=$(find_configuration TABLE)
+	PREF=$(find_configuration PREF)
+	TUN_ADDR=$(find_configuration TUN_ADDR)
+	WAIT_TIME=$(find_configuration WAIT_TIME)
+}
+
+allow_app_network()
+{
+	for UID in $(find_configuration uid)
+	do
+		iptables -t ${1} ${2} OUTPUT ${3} \
+			-w ${WAIT_TIME} \
+			-m owner \
+			--uid ${UID} \
+			-j ACCEPT
+	done
+	for UID in $(find_configuration udp_uid)
+	do
+		iptables -t ${1} ${2} OUTPUT ${3} \
+			-w ${WAIT_TIME} \
+			-p udp \
+			-m owner \
+			--uid ${UID} \
+			-j ACCEPT
+	done
+}
+
+allow_core()
+{
 	# Allow DHCP service
-	iptables -t mangle -I OUTPUT 1 \
-		-w 5 \
+	iptables -t ${1} ${2} OUTPUT ${3} \
+		-w ${WAIT_TIME} \
 		-p udp \
 		--dport 67:68 \
 		-j ACCEPT
-	iptables -t mangle -I PREROUTING 1 \
-		-w 5 \
-		-i tun+ \
-		-j ACCEPT
-	iptables -t mangle -I OUTPUT 1 \
-		-w 5 \
-		-m owner \
-		--uid ${ALLOW_UID} \
-		-j ACCEPT
-	# Allow DNS query service
-	iptables -t mangle -I OUTPUT 1 \
-		-w 5 \
-		-p udp \
-		--dport 53 \
-		-j ACCEPT
+
 	for IP in ${ALLOW_IP}
 	do
-		iptables -t mangle -I PREROUTING 1 \
-			-w 5 \
+		# Allow IP range
+		iptables -t ${1} ${2} PREROUTING ${3} \
+			-w ${WAIT_TIME} \
 			-d ${IP} \
 			-j ACCEPT
-		iptables -t mangle -I OUTPUT 1 \
-			-w 5 \
+		iptables -t ${1} ${2} OUTPUT ${3} \
+			-w ${WAIT_TIME} \
 			-d ${IP} \
 			-j ACCEPT
 	done
 	for LOOKUP in ${ALLOW_LOOKUP}
 	do
 		# Allow lookup
-		iptables -t mangle -I OUTPUT 1 \
-			-w 5 \
+		iptables -t ${1} ${2} OUTPUT ${3} \
+			-w ${WAIT_TIME} \
 			-o ${LOOKUP} \
 			-j ACCEPT
 	done
+}
 
-	for UID in $(grep -E '^[^#]' ${home_path}/.uid | \
-		grep -E -o '^uid=.+' | grep -E -o '[0-9]+ ?'
-	)
-	do
-		iptables -t mangle -A OUTPUT \
-			-w 5 \
-			-m owner \
-			--uid ${UID} \
-			-j ACCEPT
-	done
-	for UID in $(grep -E '^[^#]' ${home_path}/.uid | \
-		grep -E -o '^udp_uid=.+' | grep -E -o '[0-9]+ ?'
-	)
-	do
-		iptables -t mangle -A OUTPUT \
-			-w 5 \
-			-p udp \
-			-m owner \
-			--uid ${UID} \
-			-j ACCEPT
-	done
+v2ray_rule_1()
+{
+	iptables -t filter ${1} FORWARD ${2} \
+		-w ${WAIT_TIME} \
+		-i tun+ \
+		-j ACCEPT
+	iptables -t filter ${1} FORWARD ${2} \
+		-w ${WAIT_TIME} \
+		-o tun+ \
+		-j ACCEPT
+	iptables -t mangle ${1} PREROUTING ${2} \
+		-w ${WAIT_TIME} \
+		-i tun+ \
+		-j ACCEPT
+	iptables -t mangle ${1} OUTPUT ${2} \
+		-w ${WAIT_TIME} \
+		-m owner \
+		--uid ${ALLOW_UID} \
+		-j ACCEPT
 
-	# iptables -t mangle -A OUTPUT -w 5 \
-		# -m owner ! --uid 0-99999 -j DROP # Deny network for kernel
-	iptables -t mangle -A PREROUTING \
-		-w 5 \
+	# Allow DNS query service
+	[ ${ALLOW_LOCAL_DNS} == 1 ] && iptables -t mangle ${1} OUTPUT ${2} \
+		-w ${WAIT_TIME} \
+		-p udp \
+		--dport 53 \
+		-j ACCEPT
+	[ ${ALLOW_REMOTE_DNS} == 1 ] && iptables -t mangle ${1} PREROUTING ${2} \
+		-w ${WAIT_TIME} \
+		-p udp \
+		--dport 53 \
+		-j ACCEPT
+
+	allow_core mangle ${1} ${2}
+}
+
+v2ray_rule_2()
+{
+	allow_app_network mangle ${1}
+
+	[ ${ALLOW_REMOTE_UDP} == 1 ] && iptables -t mangle ${1} PREROUTING \
+		-w ${WAIT_TIME} \
 		-p udp \
 		-j ACCEPT
+	[ ${ALLOW_REMOTE_TCP} == 1 ] && iptables -t mangle ${1} PREROUTING \
+		-w ${WAIT_TIME} \
+		-p tcp \
+		-j ACCEPT
+	[ ${ALLOW_LOCAL_UDP} == 1 ] && iptables -t mangle ${1} OUTPUT \
+		-w ${WAIT_TIME} \
+		-p udp \
+		-j ACCEPT
+	[ ${ALLOW_LOCAL_TCP} == 1 ] && iptables -t mangle ${1} OUTPUT \
+		-w ${WAIT_TIME} \
+		-p tcp \
+		-j ACCEPT
+}
+
+tiny_rule_1()
+{
+	allow_core nat ${1} ${2}
+	allow_core mangle ${1} ${2}
+
+	iptables -t nat ${1} OUTPUT ${2} \
+		-w ${WAIT_TIME} \
+		-m owner \
+		--uid ${ALLOW_UID} \
+		-j ACCEPT
+	iptables -t mangle ${1} OUTPUT ${2} \
+		-w ${WAIT_TIME} \
+		-m owner \
+		--uid ${ALLOW_UID} \
+		-j ACCEPT
+	iptables -t mangle ${1} OUTPUT ${2} \
+		-w ${WAIT_TIME} \
+		-p tcp \
+		-m state \
+		--state NEW,ESTABLISHED,RELATED \
+		-j ACCEPT
+	iptables -t mangle ${1} OUTPUT ${2} \
+		-w ${WAIT_TIME} \
+		-p udp \
+		--dport 53 \
+		-m state \
+		--state NEW,ESTABLISHED,RELATED \
+		-j ACCEPT
+}
+
+tiny_rule_2()
+{
+	allow_app_network nat ${1}
+	allow_app_network mangle ${1}
+
+	# Begin proxy TCP
+	# iptables -t mangle ${1} OUTPUT -w ${WAIT_TIME} -m owner ! --uid 0-99999 -j DROP
+	iptables -t nat ${1} OUTPUT \
+		-w ${WAIT_TIME} \
+		-p tcp \
+		-j REDIRECT \
+		--to ${TCP_PORT}
+	# iptables -t nat ${1} OUTPUT -w ${WAIT_TIME} -p udp \
+		# --dport 53 -j REDIRECT --to 65053
+
+	# Allow DNS network
+	iptables -t nat ${1} OUTPUT \
+		-w ${WAIT_TIME} \
+		-p udp \
+		--dport 53 \
+		-j ACCEPT
+
+	iptables -t mangle -P OUTPUT ${2} -w ${WAIT_TIME}
+	# End proxy TCP
+
+	# Begin proxy forward
+	iptables -t mangle -P FORWARD ${2} -w ${WAIT_TIME}
+	ip6tables -t mangle -P FORWARD ${2} -w ${WAIT_TIME}
+	iptables -t nat ${1} PREROUTING \
+		-w ${WAIT_TIME} \
+		-p tcp \
+		-j REDIRECT \
+		--to ${TCP_PORT}
+	# iptables -t nat ${1} PREROUTING -w ${WAIT_TIME} \
+		# -p udp --dport 53 -j REDIRECT --to 65053
+
+	# Allow forward DNS network
+	iptables -t mangle ${1} FORWARD \
+		-w ${WAIT_TIME} \
+		-p udp \
+		--dport 53 \
+		-j ACCEPT
+	# End proxy forward
+}
+
+v2ray_open() {
+	echo 1 > /proc/sys/net/ipv4/ip_forward
+	echo 1 > /proc/sys/net/ipv4/ip_dynaddr
+
+	generate_uid
+
+	create_tun
+
+	v2ray_rule_1 -I 1
+
+	v2ray_rule_2 -A
+
+	# iptables -t mangle -A OUTPUT -w ${WAIT_TIME} \
+		# -m owner ! --uid 0-99999 -j DROP # Deny network for kernel
+
 	iptables -t mangle -A OUTPUT \
-		-w 5 \
+		-w ${WAIT_TIME} \
 		-j MARK \
 		--set-xmark ${MARK}
 	iptables -t mangle -A PREROUTING \
-		-w 5 \
+		-w ${WAIT_TIME} \
 		-j MARK \
 		--set-xmark ${MARK}
 
@@ -179,7 +390,7 @@ v2ray_open() {
 	ip link set up dev ${TUNDEV} qlen 1000
 	ip rule add fwmark ${MARK} lookup ${TABLE} pref ${PREF}
 	ip route add default via ${TUN_ADDR%/*} dev ${TUNDEV} table ${TABLE}
-	ip -6 rule add unreachable pref ${PREF} # Deny IPV6
+	# ip -6 rule add unreachable pref ${PREF} # Deny IPV6
 
 	mv ${0%/*}/disabled ${0%/*}/enabled && echo "v2ray" > ${0%/*}/enabled
 	echo -e "\x1b[92mV2ray Done.\x1b[0m"
@@ -190,98 +401,30 @@ v2ray_close() {
 	echo 0 > /proc/sys/net/ipv4/ip_forward
 	echo 0 > /proc/sys/net/ipv4/ip_dynaddr
 
-	iptables -t filter -D FORWARD \
-		-w 5 \
-		-i tun+ \
-		-j ACCEPT
-	iptables -t filter -D FORWARD \
-		-w 5 \
-		-o tun+ \
-		-j ACCEPT
-	# Allow DHCP service
-	iptables -t mangle -D OUTPUT \
-		-w 5 \
-		-p udp \
-		--dport 67:68 \
-		-j ACCEPT
-	iptables -t mangle -D PREROUTING \
-		-w 5 \
-		-i tun+ \
-		-j ACCEPT
-	iptables -t mangle -D OUTPUT \
-		-w 5 \
-		-m owner \
-		--uid ${ALLOW_UID} \
-		-j ACCEPT
-	# Allow DNS query service
-	iptables -t mangle -D OUTPUT \
-		-w 5 \
-		-p udp \
-		--dport 53 \
-		-j ACCEPT
-	for IP in ${ALLOW_IP}
-	do
-		iptables -t mangle -D PREROUTING \
-			-w 5 \
-			-d ${IP} \
-			-j ACCEPT
-		iptables -t mangle -D OUTPUT \
-			-w 5 \
-			-d ${IP} \
-			-j ACCEPT
-	done
-	for LOOKUP in ${ALLOW_LOOKUP}
-	do
-		# Allow lookup
-		iptables -t mangle -D OUTPUT \
-			-w 5 \
-			-o ${LOOKUP} \
-			-j ACCEPT
-	done
+	load_configuration
 
-	for UID in $(grep -E '^[^#]' ${home_path}/.uid | \
-		grep -E -o '^uid=.+' | grep -E -o '[0-9]+ ?'
-	)
-	do
-		iptables -t mangle -D OUTPUT \
-			-w 5 \
-			-m owner \
-			--uid ${UID} \
-			-j ACCEPT
-	done
-	for UID in $(grep -E '^[^#]' ${home_path}/.uid | \
-		grep -E -o '^udp_uid=.+' | grep -E -o '[0-9]+ ?'
-	)
-	do
-		iptables -t mangle -D OUTPUT \
-			-w 5 \
-			-p udp \
-			-m owner \
-			--uid ${UID} \
-			-j ACCEPT
-	done
+	v2ray_rule_1 -D
 
-	# iptables -t mangle -D OUTPUT -w 5 \
+	v2ray_rule_2 -D
+
+	# iptables -t mangle -D OUTPUT -w ${WAIT_TIME} \
 		# -m owner ! --uid 0-99999 -j DROP # Deny network for kernel
-	iptables -t mangle -D PREROUTING \
-		-w 5 \
-		-p udp \
-		-j ACCEPT
+
 	iptables -t mangle -D OUTPUT \
-		-w 5 \
+		-w ${WAIT_TIME} \
 		-j MARK \
 		--set-xmark ${MARK}
 	iptables -t mangle -D PREROUTING \
-		-w 5 \
+		-w ${WAIT_TIME} \
 		-j MARK \
 		--set-xmark ${MARK}
 
 	ip rule del pref ${PREF}
 	ip route del default dev ${TUNDEV} table ${TABLE}
-	ip -6 rule del pref ${PREF} # Allow IPV6
+	# ip -6 rule del pref ${PREF} # Allow IPV6
 	ip link set down dev ${TUNDEV}
 	ip address del ${TUN_ADDR} dev ${TUNDEV}
-	killall -9 v2ray \
+	killall v2ray \
 		thread_socket
 
 	rm -f ${home_path}/.uid
@@ -292,37 +435,9 @@ tiny_open() {
 	echo 1 > /proc/sys/net/ipv4/ip_forward
 	echo 1 > /proc/sys/net/ipv4/ip_dynaddr
 
-	echo -e "# This file is automatical" \
-	"genrated by \`mlk\`\n# DO NOT edit it\n" > ${home_path}/.uid
+	generate_uid
 
-	echo -n "uid=" >> ${home_path}/.uid
-	for PACKAGE in ${ALLOW_PACKAGES}
-	do
-		uid=$(awk "/${PACKAGE}/{print \$2}" ${PACKAGES})
-		if [ ! -z ${uid} ]
-		then
-			echo -n "${uid} " >> ${home_path}/.uid
-		fi
-	done
-	echo -e -n "\nudp_uid=" >> ${home_path}/.uid
-	for PACKAGE in ${ALLOW_UDP_PACKAGES}
-	do
-		uid=$(awk "/${PACKAGE}/{print \$2}" ${PACKAGES})
-		if [ ! -z ${uid} ]
-		then
-			echo -n "${uid} " >> ${home_path}/.uid
-		fi
-	done
-
-	if [ -c /dev/tun ]
-	then
-		[ -f /dev/net/tun ] || ( mkdir -p /dev/net \
-			&& ln -sf /dev/tun /dev/net/tun )
-	else
-		[ -c /dev/net/tun ] || ( mkdir -p /dev/net \
-			&& mknod /dev/net/tun c 10 200 \
-			&& chmod 600 /dev/net/tun )
-	fi
+	create_tun
 
 	${home_path}/thread_socket \
 		-p ${TCP_PORT} \
@@ -330,70 +445,9 @@ tiny_open() {
 		-r ${SERVER_ADDR} \
 		-d &> ${home_path}/sock.log
 
-	for UID in $(grep -E '^[^#]' ${home_path}/.uid | \
-		grep -E -o '^uid=.+' | grep -E -o '[0-9]+ ?'
-	)
-	do
-		iptables -t nat -A OUTPUT -w 5 -m owner \
-			--uid ${UID} -j ACCEPT
-		iptables -t mangle -A OUTPUT -w 5 -m owner \
-			--uid ${UID} -j ACCEPT
-	done
-	for UID in $(grep -E '^[^#]' ${home_path}/.uid | \
-		grep -E -o '^udp_uid=.+' | grep -E -o '[0-9]+ ?'
-	)
-	do
-		iptables -t nat -A OUTPUT -w 5 -m owner \
-			--uid ${UID} -p udp -j ACCEPT
-		iptables -t mangle -A OUTPUT -w 5 -m owner \
-			--uid ${UID} -p udp -j ACCEPT
-	done
-	iptables -t nat -A OUTPUT -w 5 -p udp \
-		--dport 67:68 -j ACCEPT
-	iptables -t mangle -A OUTPUT -w 5 -p udp \
-		--dport 67:68 -j ACCEPT # Allow DHCP service
+	tiny_rule_1 -I 1
 
-	for IP in ${ALLOW_IP}
-	do
-		iptables -t nat -I OUTPUT 1 -w 5 -d ${IP} -j ACCEPT
-		iptables -t mangle -I OUTPUT 1 -w 5 -d ${IP} -j ACCEPT
-		iptables -t nat -I PREROUTING 1 -w 5 -d ${IP} -j ACCEPT
-		iptables -t mangle -I PREROUTING 1 -w 5 -d ${IP} -j ACCEPT
-	done
-	for LOOKUP in ${ALLOW_LOOKUP}
-	do
-		iptables -t nat -I OUTPUT 1 -w 5 -o ${LOOKUP} -j ACCEPT
-		iptables -t mangle -I OUTPUT 1 -w 5 \
-			-o ${LOOKUP} -j ACCEPT # Allow lookup
-	done
-	iptables -t nat -I OUTPUT 1 -w 5 -m owner --uid ${ALLOW_UID} -j ACCEPT
-	iptables -t mangle -I OUTPUT 1 -w 5 -m owner --uid ${ALLOW_UID} -j ACCEPT
-
-	# Begin proxy TCP
-	# iptables -t mangle -A OUTPUT -w 5 -m owner ! --uid 0-99999 -j DROP
-	iptables -t nat -A OUTPUT -w 5 -p tcp \
-		-j REDIRECT --to ${TCP_PORT}
-	# iptables -t nat -A OUTPUT -w 5 -p udp \
-		# --dport 53 -j REDIRECT --to 65053
-	# Allow DNS network
-	iptables -t nat -A OUTPUT -w 5 -p udp --dport 53 -j ACCEPT
-	iptables -t mangle -P OUTPUT DROP -w 5
-	iptables -t mangle -I OUTPUT -w 5 -p tcp \
-		-m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-	iptables -t mangle -I OUTPUT -w 5 -p udp \
-		--dport 53 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-	# End proxy TCP
-
-	# Begin proxy forward
-	iptables -t mangle -P FORWARD DROP -w 5
-	ip6tables -t mangle -P FORWARD DROP -w 5
-	iptables -t nat -A PREROUTING -w 5 -s 192.168/16 \
-		-p tcp -j REDIRECT --to ${TCP_PORT}
-	# iptables -t nat -A PREROUTING -w 5 -s 192.168/16 \
-		# -p udp --dport 53 -j REDIRECT --to 65053
-	# Allow forward DNS network
-	iptables -t mangle -A FORWARD -w 5 -p udp --dport 53 -j ACCEPT
-	# End proxy forward
+	tiny_rule_2 -A DROP
 
 	mv ${0%/*}/disabled ${0%/*}/enabled && echo "thread_socket" > ${0%/*}/enabled
 	echo -e "\x1b[92mTiny Done.\x1b[0m"
@@ -404,72 +458,13 @@ tiny_close() {
 	echo 0 > /proc/sys/net/ipv4/ip_forward
 	echo 0 > /proc/sys/net/ipv4/ip_dynaddr
 
-	killall -9 thread_socket
+	load_configuration
 
-	for UID in $(grep -E '^[^#]' ${home_path}/.uid | \
-		grep -E -o '^uid=.+' | grep -E -o '[0-9]+ ?'
-	)
-	do
-		iptables -t nat -D OUTPUT -w 5 -m owner \
-			--uid ${UID} -j ACCEPT
-		iptables -t mangle -D OUTPUT -w 5 -m owner \
-			--uid ${UID} -j ACCEPT
-	done
-	for UID in $(grep -E '^[^#]' ${home_path}/.uid | \
-		grep -E -o '^udp_uid=.+' | grep -E -o '[0-9]+ ?'
-	)
-	do
-		iptables -t nat -D OUTPUT -w 5 -m owner \
-			--uid ${UID} -p udp -j ACCEPT
-		iptables -t mangle -D OUTPUT -w 5 -m owner \
-			--uid ${UID} -p udp -j ACCEPT
-	done
-	iptables -t nat -D OUTPUT -w 5 -p udp \
-		--dport 67:68 -j ACCEPT
-	iptables -t mangle -D OUTPUT -w 5 -p udp \
-		--dport 67:68 -j ACCEPT # Allow DHCP service
+	killall thread_socket
 
-	for IP in ${ALLOW_IP}
-	do
-		iptables -t nat -D OUTPUT -w 5 -d ${IP} -j ACCEPT
-		iptables -t mangle -D OUTPUT -w 5 -d ${IP} -j ACCEPT
-		iptables -t nat -D PREROUTING -w 5 -d ${IP} -j ACCEPT
-		iptables -t mangle -D PREROUTING -w 5 -d ${IP} -j ACCEPT
-	done
-	for LOOKUP in ${ALLOW_LOOKUP}
-	do
-		iptables -t nat -D OUTPUT -w 5 -o ${LOOKUP} -j ACCEPT
-		iptables -t mangle -D OUTPUT -w 5 \
-			-o ${LOOKUP} -j ACCEPT # Allow lookup
-	done
-	iptables -t nat -D OUTPUT -w 5 -m owner --uid ${ALLOW_UID} -j ACCEPT
-	iptables -t mangle -D OUTPUT -w 5 -m owner --uid ${ALLOW_UID} -j ACCEPT
+	tiny_rule_1 -D
 
-	# Begin proxy TCP
-	# iptables -t mangle -D OUTPUT -w 5 -m owner ! --uid 0-99999 -j DROP
-	iptables -t nat -D OUTPUT -w 5 -p tcp \
-		-j REDIRECT --to ${TCP_PORT}
-	# iptables -t nat -D OUTPUT -w 5 -p udp \
-		# --dport 53 -j REDIRECT --to 65053
-	# Allow DNS network
-	iptables -t nat -D OUTPUT -p udp --dport 53 -j ACCEPT
-	iptables -t mangle -P OUTPUT ACCEPT -w 5
-	iptables -t mangle -D OUTPUT -w 5 -p tcp \
-		-m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-	iptables -t mangle -D OUTPUT -w 5 -p udp \
-		--dport 53 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-	# End proxy TCP
-
-	# Begin proxy forward
-	iptables -t mangle -P FORWARD ACCEPT -w 5
-	ip6tables -t mangle -P FORWARD ACCEPT -w 5
-	iptables -t nat -D PREROUTING -w 5 -s 192.168/16 \
-		-p tcp -j REDIRECT --to ${TCP_PORT}
-	# iptables -t nat -D PREROUTING -w 5 -s 192.168/16 \
-		# -p udp --dport 53 -j REDIRECT --to 65053
-	# Allow forward DNS network
-	iptables -t mangle -D FORWARD -w 5 -p udp --dport 53 -j ACCEPT
-	# End proxy forward
+	tiny_rule_2 -D ACCEPT
 
 	rm -f ${home_path}/.uid
 	mv ${0%/*}/enabled ${0%/*}/disabled
@@ -485,6 +480,9 @@ then
 		elif [ ${1} == "v" ]
 		then
 			v2ray_open
+		elif [ 's' == ${1} ]
+		then
+			echo 'MLKit is stopped.'
 		else
 			echo "Undefined core."
 			exit -1
@@ -526,6 +524,9 @@ then
 				echo "Undefined error."
 				exit -2
 			fi
+		elif [ 's' == ${1} ]
+		then
+			echo "MLKit is running. (${status})"
 		else
 			echo "Selecting core is invalid."
 			exit -3
@@ -539,3 +540,4 @@ else
 	exit 1
 fi
 exit 127
+
